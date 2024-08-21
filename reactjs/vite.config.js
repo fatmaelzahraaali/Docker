99b6688304a7b7e1a6dafcba90d7d8370f675840
fatmaelzahraaali <fatma.elzahraa.ali.75@gmail.com>
2024-08-22 00:18:04 +0300

    config edit

--- reactjs/vite.config.js
+++ reactjs/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  server: {
+    host: '0.0.0.0',  // Listen on all interfaces
+    port: 8080,        // Ensure this matches the port exposed in Docker
+  },
+};
